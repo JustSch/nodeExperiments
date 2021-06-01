5805bad4df4ab8e4fc3709677e57c33edf82983e
Justin Schreiber <jschreiber234@gmail.com>
2021-06-01 19:13:45 -0400

    initial login aja layout nodels login page etc

---
+++ loginTutorial/routes/users.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../models/user.js")
+
+router.get('/login',(req,res) =>{
+    res.render('login');
+});
+router.get('/register',(req,res)=>{
+    res.render('register')
+});
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+  
+    if (!name || !email || !password || !password2) {
+      errors.push({ msg: 'Please enter all fields' });
+    }
+  
+    if (password != password2) {
+      errors.push({ msg: 'Passwords do not match' });
+    }
+  
+    if (password.length < 6) {
+      errors.push({ msg: 'Password must be at least 6 characters' });
+    }
+  
+    if (errors.length > 0) {
+      res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        password2
+      });
+    } else {
+      User.findOne({ email: email }).then(user => {
+        if (user) {
+          errors.push({ msg: 'Email already exists' });
+          res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+          });
+        } else {
+          const newUser = new User({
+            name,
+            email,
+            password
+          });
+        }
+    });
+}
+   
+         
+});
+
+router.post('/login',(req,res) => {
+});
+
+router.get('/logout',(req,res) =>{
+});
+
+module.exports = router;
