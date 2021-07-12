de34a53abaefcef949e026e4b682851f52a3bfc3
Justin Schreiber <jschreiber234@gmail.com>
2021-07-12 18:10:11 -0400

    added example with wildcard

--- example2/server.js
+++ example2/server.js
@@ -9,6 +9,17 @@ app.get('/listUsers',function (req, res){
     })
 })
 
+
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      var users = JSON.parse( data );
+      var user = users["user" + req.params.id] 
+      //console.log( user.name );
+      res.end(user.name);
+   });
+})
+
 var server = app.listen(8081, "127.0.0.1", function(){
     var host = server.address().address;
     var port = server.address().port;
